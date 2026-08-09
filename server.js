f16433e304e940dd5cd7022f82789d38eae001e4
gw410551-netizen <gw410551@gmail.com>
2026-08-09 14:35:34 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const app = express();
+app.use(cors()); // السماح بالاتصال من أي مكان
 const fs = require('fs');
 const axios = require('axios');
 
