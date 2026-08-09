a69d9c439d81a9779030f7af02fa07cebff9ae9e
gw410551-netizen <gw410551@gmail.com>
2026-08-09 14:08:53 +0300

    Update server.js

--- server.js
+++ server.js
@@ -74,7 +74,7 @@ app.post('/api/results', async (req, res) => {
 // خدمة ملفات الموقع
 app.use(express.static('public'));
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
