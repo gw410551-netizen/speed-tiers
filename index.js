9246f5fc09c23bd9d0bef7ecc29ef084efef07c4
gw410551-netizen <gw410551@gmail.com>
2026-08-09 13:49:55 +0300

    Update index.js

--- index.js
+++ index.js
@@ -78,7 +78,7 @@ client.on('messageCreate', async message => {
     message.reply('✅ تم تسجيل النتيجة وسحب السكن وإعطاء الرتبة بنجاح!');
     // إرسال البيانات لسيرفر الـ Backend لتظهر في الموقع
     try {
-        await fetch('http://localhost:8080/api/results', {
+        await fetch('https://speed-tiers-discordtoken.up.railway.app/api/results', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({
